1766b6f1d6c4fcb665943e32e4516442ff50383c
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2025-06-14 10:28:43 -0300

    update

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -31,6 +31,72 @@ const mockPatients = [
             Jantar: "Salada verde, peito de frango grelhado"
         },
         observacoes: "Monitorar aceitação da dieta."
+    },
+    {
+        nome: "João Silva",
+        prontuario: "123456",
+        enfermaria: "Enf. 3, Leito 12",
+        nivelAssistencia: "Nível I (Primário)",
+        peso: 70,
+        altura: 1.75,
+        imc: 22.86,
+        dieta: "Dieta Geral",
+        refeicoes: {
+            Desjejum: "Café com leite, pão integral, margarina",
+            Almoço: "Arroz, feijão, frango grelhado, salada",
+            Jantar: "Sopa de legumes, pão integral"
+        },
+        observacoes: "Paciente aceita bem a dieta."
+    },
+    {
+        nome: "Maria Oliveira",
+        prontuario: "789012",
+        enfermaria: "Enf. 5, Leito 8",
+        nivelAssistencia: "Nível II (Secundário)",
+        peso: 60,
+        altura: 1.67,
+        imc: 21.48,
+        dieta: "Dieta Hipossódica",
+        refeicoes: {
+            Desjejum: "Suco natural, pão sem sal, queijo cottage",
+            Almoço: "Arroz integral, peixe cozido, legumes no vapor",
+            Lanche: "Fruta fresca",
+            Jantar: "Salada verde, peito de frango grelhado"
+        },
+        observacoes: "Monitorar aceitação da dieta."
+    },
+    {
+        nome: "João Silva",
+        prontuario: "123456",
+        enfermaria: "Enf. 3, Leito 12",
+        nivelAssistencia: "Nível I (Primário)",
+        peso: 70,
+        altura: 1.75,
+        imc: 22.86,
+        dieta: "Dieta Geral",
+        refeicoes: {
+            Desjejum: "Café com leite, pão integral, margarina",
+            Almoço: "Arroz, feijão, frango grelhado, salada",
+            Jantar: "Sopa de legumes, pão integral"
+        },
+        observacoes: "Paciente aceita bem a dieta."
+    },
+    {
+        nome: "Maria Oliveira",
+        prontuario: "789012",
+        enfermaria: "Enf. 5, Leito 8",
+        nivelAssistencia: "Nível II (Secundário)",
+        peso: 60,
+        altura: 1.67,
+        imc: 21.48,
+        dieta: "Dieta Hipossódica",
+        refeicoes: {
+            Desjejum: "Suco natural, pão sem sal, queijo cottage",
+            Almoço: "Arroz integral, peixe cozido, legumes no vapor",
+            Lanche: "Fruta fresca",
+            Jantar: "Salada verde, peito de frango grelhado"
+        },
+        observacoes: "Monitorar aceitação da dieta."
     }
 ];
 
